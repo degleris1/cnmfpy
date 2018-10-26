gradients are correct up to a scalar, but bcd always converges to a trivial solution

tests/test_grads.py
@@ -3,7 +3,7 @@ import numpy as np
 from cmfpy.utils import ModelDimensions
 from cmfpy.optimize import compute_gH, compute_gW, compute_loss
 from cmfpy.datasets import Synthetic, SongbirdHVC
-from scipy.optimize import check_grad
+from scipy.optimize import check_grad, approx_fprime
 
 TOL = 1e-4
 SEED = 1234
@@ -32,25 +32,43 @@ def test_gradients(data, L, K):
 
     # Check gradient for W. Thinly wrap internal functions to deal with
     # raveled parameter vectors.
-    def f_W(w_vec):
+    def loss_W(w_vec):
         w = w_vec.reshape(W.shape)
         return compute_loss(data, w, H)
 
-    def g_W(w_vec):
+    def grad_W(w_vec):
         w = w_vec.reshape(W.shape)
-        return compute_gW(data, w, H)[1].ravel()
+        return compute_gW(data, w, H).ravel()
 
-    err = check_grad(f_W, g_W, W.ravel())
-    assert err < (TOL * np.sqrt(W.size))
+    eps = np.sqrt(np.finfo(float).eps)
+
+    approx_grad = approx_fprime(W.ravel(), loss_W, eps)
+    approx_grad /= np.linalg.norm(approx_grad)
+
+    grad = compute_gW(data, W, H).ravel()
+    grad /= np.linalg.norm(grad)
+
+    assert np.linalg.norm(grad - approx_grad) < TOL, "W failed for " + str(eps)
+
+    # err = check_grad(f_W, g_W, W.ravel())
+    # assert err < (TOL * np.sqrt(W.size))
 
     # Check gradient for H.
-    def f_H(h_vec):
+    def loss_H(h_vec):
         h = h_vec.reshape(H.shape)
         return compute_loss(data, W, h)
 
-    def g_H(h_vec):
+    def grad_H(h_vec):
         h = h_vec.reshape(H.shape)
-        return compute_gW(data, W, h)[1].ravel()
+        return compute_gW(data, W, h).ravel()
 
-    err = check_grad(f_H, g_H, H.ravel())
-    assert err < (TOL * np.sqrt(H.size))
+    approx_grad = approx_fprime(H.ravel(), loss_H, eps)
+    approx_grad /= np.linalg.norm(approx_grad)
+
+    grad = compute_gH(data, W, H).ravel()
+    grad /= np.linalg.norm(grad)
+
+    assert np.linalg.norm(grad - approx_grad) < TOL, "H failed"
+
+    # err = check_grad(f_H, g_H, H.ravel())
+    # assert err < (TOL * np.sqrt(H.size))