0f31f7f9f00b7a7c2647c6f4615e1453d26e6144
Anthony Degleris <degleris1@gmail.com>
2018-11-21 11:06:47 -0500

    Added H update to SimpleHALS. Basic version of HALS (with the simple H update) now fully functional using AbstractOptimizer.
    Note: To be consistent with other algorithms, the residuals were changed from X - X_hat to X_hat - X. Accordingly, anywhere `resids` were used, we now use `-resids`.

--- cmfpy/algs/hals.py
+++ cmfpy/algs/hals.py
@@ -40,10 +40,39 @@ class SimpleHALSUpdate(AbstractOptimizer):
                 self.resids += np.outer(self.W[l, :, k], H_unfold[ind, :])
 
     def update_H(self):
-        pass
+        L, N, K = self.W.shape
+        T = self.H.shape[1]
+
+        # Set up residual and norms of each column
+        W_norms = la.norm(self.W, axis=1).T  # K * L, norm along N
+
+        # Update each component
+        for k in range(K):
+            Wk = self.W[:, :, k].T  # TODO: will this slow things down?
+
+            # Update each timebin
+            for t in range(T):
+                # TODO could use some help with indentation here
+                resid_slice = (self.resids[:, t:t+L]
+                               - self.H[k, t] * Wk[:, :T-t])
+
+                norm_Wkt = np.sqrt(np.sum(W_norms[k, :T-t]**2))
+                self.H[k, t] = self.new_H_entry(Wk[:, :T-t],
+                                                norm_Wkt,
+                                                resid_slice)
+
+                self.resids[:, t:t+L] = (resid_slice
+                                         + self.H[k, t] * Wk[:, :T-t])
 
     def new_W_col(self, Hkl, norm_Hkl, resid):
         """
         """
         # TODO reconsider transpose
         return np.maximum(np.dot(-resid, Hkl) / (norm_Hkl**2 + EPSILON), 0)
+
+    def new_H_entry(self, Wkt, norm_Wkt, resid_slice):
+        """
+        """
+        trace = np.dot(np.ravel(Wkt), np.ravel(-resid_slice))
+
+        return np.maximum(trace / (norm_Wkt**2 + EPSILON), 0)
